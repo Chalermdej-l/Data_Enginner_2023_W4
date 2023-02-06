7582803ffca6546c5022dd21e982344cf5a3f61b
Chalermdej Lematavekul <96781714+Chalermdej-l@users.noreply.github.com>
2023-02-06 21:40:04 +0700

    Update etl_web_to_gcs.py

--- etl_web_to_gcs.py
+++ etl_web_to_gcs.py
@@ -1,55 +1,61 @@
-from pathlib import Path
-import pandas as pd
 from prefect import flow,task
 from prefect_gcp.cloud_storage import GcsBucket
-
+from prefect_gcp import GcpCredentials
+from pathlib import Path
+import pandas as  pd
 @task()
-def fetch(url_file):
-    df = pd.read_csv(url_file)
-    return df
-
-@task(log_prints=True)
-def clean(df):
-    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
-    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
-
-    return df
-
-task()
-def writelocal(df,color,dfile):
-    path = Path(f'data/{color}/{dfile}.parquet')
-    print(path)
-    df.to_parquet(path, compression='gzip')
-    return path
-
-@task()
-def writegcs(path):
+def extract_from_gcs(color,year,month):
+    gcs_path =f'data/{color}/{color}_tripdata_{year}-{month:02}.parquet'
     gcs_block = GcsBucket.load("test-gcs-bigger124")
-    gcs_block.upload_from_path(
-    from_path = path,
-    to_path = path
-    )
-    return
+    gcs_block.get_directory(from_path=gcs_path,local_path=f'./data/')
+    return Path(f'./{gcs_path}')
 
-@flow(name='what',log_prints=True)
-def etl_web_to_gcs(color,year,month):
-    dataset_file =f'{color}_tripdata_{year}-{month:02}'
-    url_file = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz'
+@task()
+def loaddata(path):
+    df = pd.read_parquet(path)
 
-    df = fetch(url_file)
-    df = clean(df)
-    print(f'There are total of {len(df)} rows in the data.')
-    path = writelocal(df,color,dataset_file)
-    writegcs(path) 
+
     return df
 
-@flow(name='parent_fcs')
-def parentflow_github(color='yellow',year=2019,month=3) -> None:
-    etl_web_to_gcs(color,year,month)
+@task()
+def write_bq(df: pd.DataFrame):
+
+    gcp_credentials_block = GcpCredentials.load("test-gcp-cred")
+
+    df.to_gbq (
+    destination_table='test_bigger124test.yellow_2019',
+    project_id='quantum-plasma-376313',
+    credentials=gcp_credentials_block.get_credentials_from_service_account(),
+    chunksize=500_000,
+    if_exists='append'
+    )
+
+
+@flow(name='bq',log_prints=True)
+def etl_gcs_to_bq(color,year,months):
+    total_row = 0
+    for month in months:
+        path = extract_from_gcs(color,year,month)
+        df = loaddata(path)
+        rows = len(df)
+        print(f'Loading total of {rows} rows.')
+        total_row +=rows 
+        write_bq(df)
+
+    print(f'Total row proceed {total_row}.')    
     return None
+
+@flow(name='parent')
+def parentflow(color,year,month):
+    color='yellow'
+    year='2019'
+    month =[2,3]    
+    etl_gcs_to_bq(color,year,month)
+
 
 if __name__ == "__main__":
     color='yellow'
     year='2019'
-    month =3   
-    parentflow_github(color,year,month)
+    month =[2,3]    
+    path = parentflow(color,year,month)
+    
